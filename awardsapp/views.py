e715c27e11853274a4af7a7c7f437b989132a51c
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-12-12 12:07:45 +0300

    update views

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from awardsapp import *
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def index(request):
+    return render(request,'index.html')
