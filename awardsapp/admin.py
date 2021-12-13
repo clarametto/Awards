783f0d327e06349a25e3498dc3102f6e0dfb03a6
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-12-13 21:24:40 +0300

    create admin

--- awardsapp/admin.py
+++ awardsapp/admin.py
@@ -1,3 +1,8 @@
+from typing import Callable
 from django.contrib import admin
-
+from .models import Category, Location, Profile, Project
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Category)
+admin.site.register(Location)
