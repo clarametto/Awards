71e1eeb26b1d30f7065358f27ff1f754ed93bf6f
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-12-14 21:26:44 +0300

    add class profile in views

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -124,5 +124,10 @@ class ProjectList(APIView):
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects,many=True)
         return Response(serializer.data)
-
+class ProfileList(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get(self,request,format=None):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles,many=True)
+        return Response(serializer.data)
 
