cf80310a65604bf444ed198069f77df407aac184
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-12-15 18:40:53 +0300

    test the project class

--- awardsapp/tests.py
+++ awardsapp/tests.py
@@ -30,3 +30,31 @@ class ProfileTestClass(TestCase):
         self.profile.create_profile()
         profiles = Profile.objects.all()
         self.assertTrue(len(profiles) > 0)
+
+class ProjectTestClass(TestCase):
+    def setUp(self):
+
+        user = User.objects.create(
+            username="test_user", first_name="clara", last_name="metto"
+        )
+        self.project = Project(
+            title="Test Photos",
+            description="Test Category",
+            photo="image.png",
+            user=user,
+        )
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project, Project))
+    def test_save_method(self):
+        self.project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) > 0)
+    def test_update_project(self):
+        self.project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) > 0)
+    def test_delete_method(self):
+        self.project.save_project()
+        self.project.delete_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) == 0)
