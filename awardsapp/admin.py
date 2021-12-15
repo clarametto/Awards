259eaade02bf2628bab2027011a87a6c5ad67754
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-12-15 14:09:45 +0300

    modify admin

--- awardsapp/admin.py
+++ awardsapp/admin.py
@@ -1,8 +1,6 @@
 from typing import Callable
 from django.contrib import admin
-from .models import Category, Location, Profile, Project
+from .models import  Profile, Project
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Profile)
-admin.site.register(Category)
-admin.site.register(Location)
